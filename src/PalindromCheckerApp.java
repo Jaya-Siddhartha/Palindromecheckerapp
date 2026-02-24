6203cb78a9d25d5afb24388e6cf44d426ab465d7
Jaya-Siddhartha <jayasiddhartha5@gmail.com>
2026-02-24 14:56:57 +0530

    UC2:madam palindrome

--- src/PalindromCheckerApp.java
+++ src/PalindromCheckerApp.java
@@ -1,7 +1,19 @@
 public class PalindromCheckerApp {
-    public static void main(String[] args) {
-        System.out.println("welcome ");
-        System.out.println("version 1");
-        System.out.println("system in initialized successfully");
+    // Method to check if a string is palindrome
+        public static boolean isPalindrome(String str) {
+            String cleanStr = str.replaceAll("\\s+", "").toLowerCase();
+            StringBuilder reversed = new StringBuilder(cleanStr).reverse();
+            return cleanStr.equals(reversed.toString());
+        }
+
+        // main method - entry point
+        public static void main(String[] args) {
+            String hardcodedWord = "madam"; // hardcoded string
+
+            if (isPalindrome(hardcodedWord)) {
+                System.out.println(hardcodedWord + " is a palindrome.");
+            } else {
+                System.out.println(hardcodedWord + " is NOT a palindrome.");
+            }
+        }
     }
-}
