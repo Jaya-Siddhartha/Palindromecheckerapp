5b4eed62f35b51927665b3404619f621ba74fe20
Jaya-Siddhartha <jayasiddhartha5@gmail.com>
2026-02-24 14:49:16 +0530

    UC1:added welocome message

--- src/PalindromCheckerApp.java
+++ src/PalindromCheckerApp.java
@@ -1,2 +1,7 @@
 public class PalindromCheckerApp {
+    public static void main(String[] args) {
+        System.out.println("welcome ");
+        System.out.println("version 1");
+        System.out.println("system in initialized successfully");
+    }
 }
